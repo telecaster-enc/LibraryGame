6ab640802b2c5c7ab1f5b0879ec013df0829942c
telecaster-enc <galangmuda1507@gmail.com>
2026-06-29 07:59:52 +0700

    small opt, brb

--- src/main.py
+++ src/main.py
@@ -6,16 +6,16 @@ pygame.display.set_caption("Library Game")
 pygame.display.set_icon(pygame.image.load("assets/images/icon.jpg"))
 clock = pygame.time.Clock()
 
-tot_buku = 9
-current_time = random.uniform(0, math.pi*2)
-books_inhand = 0
-total_books_collected = 0
-num_slot = 0
 running = True
 warna = "grey"
 screen.fill(warna)
 text_font = pygame.font.SysFont(None, 36)
 text_small = pygame.font.SysFont(None, 24)
+tot_buku = 9
+current_time = 0
+books_inhand = 0
+total_books_collected = 0 
+num_slot = 0
 
 class rak_sprite(pygame.sprite.Sprite):
     def __init__(self):
@@ -25,11 +25,6 @@ class rak_sprite(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 1280-400
         self.rect.y = 720-400
-
-class shadow(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.draw.ellipse
 
 class buku_sprite_yellow(pygame.sprite.Sprite):
     def __init__(self):
@@ -77,7 +72,7 @@ class buku_sprite_white(pygame.sprite.Sprite):
         self.image = pygame.image.load("assets/images/buku_putih.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, 1180)
+        self.rect.x = random.randint(100,1280-400-100)
         self.rect.y = random.randint(0, 620)
         self.base_y= self.rect.y
         self.held=False 
@@ -97,11 +92,12 @@ class kokomi(pygame.sprite.Sprite):
         self.image = pygame.image.load("assets/images/icon.jpg")
         self.image = pygame.transform.scale(self.image, (100, 100))
         self.image = pygame.transform.flip(self.image, True, False)
+        self.turn_left = False
         self.rect = self.image.get_rect()
         self.rect.x = 100
         self.rect.y = 100
 
-    def update(self): #Movement of the player
+    def update(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.rect.y += 10
@@ -109,9 +105,15 @@ class kokomi(pygame.sprite.Sprite):
             self.rect.y -= 10
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rect.x += 10
+            if self.turn_left:
+                self.image = pygame.transform.flip(self.image, True, False)
+                self.turn_left = False
             #self.rect = self.image.get_rect()
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rect.x -= 10
+            if not self.turn_left:
+                self.image = pygame.transform.flip(self.image, True, False)
+                self.turn_left = True
             #self.rect = self.image_flipped.get_rect()
 
 Kokomi = kokomi()
@@ -130,10 +132,11 @@ for i in range(tot_buku//3):
     object_sprites.add(buku_banyak)
 rak_sprites = pygame.sprite.GroupSingle(rak)
 
-text_surface = text_font.render("Collect the books!", True, (255, 255, 255))
-text_total = text_font.render(f"Total books collected: 0/{tot_buku}", True, (255, 255, 255))
+text_surface = text_font.render("Collect the books!", True, ("white"))
+text_total = text_font.render(f"Total books collected: 0/{tot_buku}", True, ("white"))
 text_inventory = text_font.render(f"Inventory: {books_inhand}/3", True, ("white"))
 text_pickup = text_small.render("Press [E] to pick up the book", True, ("white"))
+text_total = text_font.render(f"Total books collected: {total_books_collected}/{tot_buku}", True, ("white"))
 
 def draw_everything():
     screen.fill(warna)
@@ -141,20 +144,16 @@ def draw_everything():
     object_sprites.draw(screen)
     player_sprites.draw(screen)
     book_in_hand.draw(screen)
+    player_sprites.update()
+    object_sprites.update()
+    book_in_hand.update()
     screen.blit(text_surface, (10, 10))
     screen.blit(text_inventory, (10, 720 - text_total.get_height()-10 - text_inventory.get_height() - 10))
     screen.blit(text_total, (10, 720 - text_total.get_height()-10))
 
-text_total = text_font.render(f"Total books collected: {total_books_collected}/{tot_buku}", True, (255, 255, 255))
-
 while running: 
-    player_sprites.update()
-    object_sprites.update()
-    book_in_hand.update()
     draw_everything()
     current_time+=0.05
-
-    current_time_ms = pygame.time.get_ticks()
 
     collision_book = pygame.sprite.spritecollide(Kokomi, object_sprites, False)
     if collision_book:
